a052022cbbc6e2a06cc60103e5806902646baa7b
a <a>
2014-02-01 12:19:38 +0200

    FigureTypes

--- src/Chess/Figure/FigureTypes/Pawn.java
+++ src/Chess/Figure/FigureTypes/Pawn.java
@@ -1,12 +1,11 @@
-package Chess.Figure;
+package Chess.Figure.FigureTypes;
 
 /**
  * Created with IntelliJ IDEA.
  * User: java
  * Date: 14.1.2
- * Time: 12:15
+ * Time: 12:18
  * To change this template use File | Settings | File Templates.
  */
-public class Figure {
-
+public class Pawn {
 }
