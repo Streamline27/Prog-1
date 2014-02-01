d689338caa9523a9ef04350062098f7b1998ce73
a <a>
2014-02-01 12:29:58 +0200

    FigureTypes

--- src/Chess/Board/BoardCell.java
+++ src/Chess/Board/BoardCell.java
@@ -8,4 +8,18 @@ package Chess.Board;
  * To change this template use File | Settings | File Templates.
  */
 public class BoardCell {
+    public static enum CellColor {BLACK, WHITE};
+    public CellColor currentCellColor;
+
+    public byte CellPosition;
+
+    public BoardCell(byte position)
+    {
+        if (position % 2 == 0)
+            this.currentCellColor = CellColor.BLACK;
+        else
+            this.currentCellColor = CellColor.WHITE;
+
+        this.CellPosition = position;
+    }
 }
